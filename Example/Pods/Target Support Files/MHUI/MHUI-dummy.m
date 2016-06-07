d4c40acdc4afc7172ba6e187ae5dd9796f06da22
Malcolm Hall <malhal@users.noreply.github.com>
2016-06-07 18:52:56 +0100

    Commit

--- Example/Pods/Target Support Files/MHUI/MHUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MHUI : NSObject
-@end
-@implementation PodsDummy_MHUI
-@end
--- MHUI/MHUI.h
+++ MHUI/MHUI.h
@@ -2,10 +2,19 @@
 //  MHUI.h
 //  MHUI
 //
-//  Created by Malcolm Hall on 06/06/2016.
-//
+//  Created by Malcolm Hall on 07/06/2016.
+//  Copyright © 2016 Malcolm Hall. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHUI.
+FOUNDATION_EXPORT double MHUIVersionNumber;
+
+//! Project version string for MHUI.
+FOUNDATION_EXPORT const unsigned char MHUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHUI/PublicHeader.h>
 #import <MHUI/MHUDictionaryViewController.h>
 #import <MHUI/MHUFlipController.h>
 
