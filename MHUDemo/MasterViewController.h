b60341ccadf0de9e50974496ed2b872c5245f3fa
Malcolm Hall <malhal@users.noreply.github.com>
2016-09-19 22:08:17 +0100

    Demo added

---
+++ MHUDemo/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MHUDemo
+//
+//  Created by Malcolm Hall on 19/09/2016.
+//  Copyright © 2016 Malcolm Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ MHUDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MHUDemo
+//
+//  Created by Malcolm Hall on 19/09/2016.
+//  Copyright © 2016 Malcolm Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
